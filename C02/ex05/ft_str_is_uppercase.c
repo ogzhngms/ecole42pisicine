03bf1b8e1c9903d7564a812eb9346755e7b1a0de
ogzhngms <71395893+ogzhngms@users.noreply.github.com>
2022-12-15 00:14:22 +0300

    Update ft_str_is_uppercase.c

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edogrul <enes.dogrul41@gmail.com>          +#+  +:+       +#+        */
+/*   By: oggumus <oguzhan.gumus.08@hotmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/13 16:44:20 by edogrul           #+#    #+#             */
-/*   Updated: 2021/10/13 16:44:25 by edogrul          ###   ########.tr       */
+/*   Created: 2022/10/13 10:40:09 by oggumus           #+#    #+#             */
+/*   Updated: 2022/10/13 11:06:02 by oggumus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_str_is_uppercase(char *str)
 {
-	int		i;
+	int	i;
 	char	b;
 	bool	sonuc;
 
