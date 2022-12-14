07eb122a740729aa5bdc912231fc520d3ab783ee
ogzhngms <71395893+ogzhngms@users.noreply.github.com>
2022-12-14 04:19:35 +0300

    Update ft_print_comb.c

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oggumus <oguzhan.gumus.08@hotmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -42,22 +42,5 @@ void	ft_print_comb(void)
 			b++;
 		}
 		a++;
-	}
-}
-/////////////////////OR////////////////////
-
-
-#include <unistd.h>
-
-void	ft_putchar(char c) 
-{
-	write(1, &c, 1);
-}
-
-void	ft_print_alphabet(void) {
-	char i = 'a';
-	while(i <= 'z') {
-		ft_putchar(i);
-		i++;
 	}
 }
